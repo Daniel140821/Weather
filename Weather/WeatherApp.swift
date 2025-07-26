af520cf1c1ea97df4c7266fc482bb369a459581e
Daniel <danielchun821@gmail.com>
2025-07-26 12:33:51 +0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Daniel on 26/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Daniel on 26/7/2025.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
